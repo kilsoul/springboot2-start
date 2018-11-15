ab4739ce27a37912879a64ec73d13a37dc53fd21
qingkun.zeng <qingkun.zeng@alo7.com>
2018-11-15 20:47:58 +0800

    Initial commit

---
+++ src/main/java/com/fightermap/start/Springboot2StartApplication.java
@@ -0,0 +1,12 @@
+package com.fightermap.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2StartApplication.class, args);
+    }
+}
